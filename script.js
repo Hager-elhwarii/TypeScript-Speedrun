875191679ac3014087c7b51e3cd531b42aaefd93
hager-dot <hagerelhwarii6633@gmail.com>
2024-03-02 18:02:42 +0200

    :sparkles: Use TypeScript compiler

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = {
+    name: "alii",
+    age: 5
+};
+console.log(user.name);
+console.log(user.age);
---
+++ script.ts
@@ -0,0 +1,15 @@
+export interface User {
+    name: string,
+    age: number
+}
+
+const user: User = {
+  name: "alii",
+  age: 5
+}
+
+
+console.log(user.name);
+console.log(user.age);
+
+
